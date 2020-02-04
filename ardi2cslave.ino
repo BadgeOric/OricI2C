130627cc8b741d392aa60925f8b0d2fa61c3d7ee
BadgeOric <43514521+BadgeOric@users.noreply.github.com>
2020-02-04 20:58:39 +0000

    Update ardi2cslave.ino

--- ardi2cslave.ino
+++ ardi2cslave.ino
@@ -1,4 +1,4 @@
-// Wire Slave Receiver
+/// Wire Slave Receiver
 // by Nicholas Zambetti <http://www.zambetti.com>
 
 // Demonstrates use of the Wire library
@@ -11,17 +11,36 @@
 
 
 #include <Wire.h>
-
+int incomingByte = 0; // for incoming serial data
+int bufflen = 0;// count of serial inputs
+int mybuff[33]={};
+int curpos=0;
+int bufffull=0;
 void setup()
 {
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent);
   Serial.begin(9600);           // start serial for output
 }
 
 void loop()
 {
-  delay(100);
+  if (Serial.available()>0) {
+          if(bufffull==1){
+            // do nothing
+                      }
+          else
+          {
+            incomingByte=Serial.read();
+            mybuff[curpos]=incomingByte;
+            Serial.write(incomingByte);
+            curpos++;
+            if(curpos>32) {bufffull=1;}
+          }
+      }
+  
+
 }
 
 // function that executes whenever data is received from master
@@ -34,6 +53,23 @@ void receiveEvent(int howMany)
     Serial.print(c);         // print the character
   }
   int x = Wire.read();    // receive byte as an integer
-  Serial.write(x);         // print the integer
-  if (x==13) { Serial.write("\r\n"); }
+  if (x!=0){
+  Serial.write(x);         // print the integer as a character
+  if (x==13) { Serial.write("\r\n"); }    //cr and lf
+
+  }
+  
+}
+
+
+void requestEvent()
+{
+  for (byte i = 0; i < 32; i = i + 1) {
+    if(i<=curpos){
+          Wire.write(mybuff[i]);
+          mybuff[i]=0;
+          }        
+      }
+      curpos=0;
+      bufffull=0;  
 }
